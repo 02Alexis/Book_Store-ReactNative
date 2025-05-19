6bff2dad08211e10121c4dc4e1c9a977b02845e4
02Alexis <m_chael1012@hotmail.com>
2025-05-19 12:25:00 -0500

    react native & configuración de entorno

---
+++ mobile/app/index.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.title}>Hola Mundo.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    color: "green",
+  },
+});
